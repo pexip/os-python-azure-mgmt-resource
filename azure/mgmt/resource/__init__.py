84ef9dc76dae1ce79fc19c4df8c6bc52c6aaac30
John-Mark Bell <jmb@pexip.com>
2017-02-28 11:44:13 +0000

    Imported Upstream version 0.31.0

--- azure/mgmt/resource/__init__.py
+++ azure/mgmt/resource/__init__.py
@@ -1,1 +1,11 @@
-﻿__import__('pkg_resources').declare_namespace(__name__)
+﻿from .features import FeatureClient
+from .locks import ManagementLockClient
+from .policy import PolicyClient
+from .resources import ResourceManagementClient
+from .subscriptions import SubscriptionClient
+from .links import ManagementLinkClient
+
+from .version import VERSION
+
+__version__ = VERSION
+
